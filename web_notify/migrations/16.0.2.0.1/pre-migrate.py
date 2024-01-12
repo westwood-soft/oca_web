070b854a06f2c17ff3d35027a6ede68af73f3409
Michael Dahl <michael@dahl.software>
2024-01-12 17:48:44 +0100

    Remove assets

---
+++ web_notify/migrations/16.0.2.0.1/pre-migrate.py
@@ -0,0 +1,39 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+            SELECT res_id
+              FROM ir_model_data d
+             WHERE NOT EXISTS (SELECT 1
+                                 FROM ir_model_data
+                                WHERE id != d.id
+                                  AND res_id = d.res_id
+                                  AND model = d.model
+                                  AND module != d.module)
+               AND module = 'web_notify'
+               AND model = 'ir.ui.view'
+          ORDER BY id DESC
+            """
+    )
+    for view_id in cr.fetchall():
+        util.remove_view(cr, view_id=view_id)
+
+    cr.execute(
+        """
+            SELECT res_id
+              FROM ir_model_data d
+             WHERE NOT EXISTS (SELECT 1
+                                 FROM ir_model_data
+                                WHERE id != d.id
+                                  AND res_id = d.res_id
+                                  AND model = d.model
+                                  AND module != d.module)
+               AND module = 'web_notify'
+               AND model = 'ir.ui.menu'
+          ORDER BY id DESC
+    """
+    )
+    menu_ids = cr.fetchall()
+    util.remove_menus(cr, menu_ids)
